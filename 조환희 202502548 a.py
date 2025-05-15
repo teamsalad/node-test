f842ac58a4a53d561fdf56217297dd8076be8020
teamsalad <108782836+teamsalad@users.noreply.github.com>
2025-05-16 00:34:49 +0900

    Add files via upload

---
+++ 조환희 202502548 a.py
@@ -0,0 +1,16 @@
+for i in range(2,10): #단수
+    for j in range(1,10): #곱하는 수
+        print(i,"*",j,"=",i*j)
+        if j==9:
+            print() #단 사이 한 줄 뛰우기
+
+#
+#dan=int(input("원하는 단수 입력: "))
+#if dan > 1 and dan < 10:
+ #   for i in range(1,10):
+  #      print(dan,"*",i,"=",dan*i)
+
+#else:
+ #   print("잘못된 단수 입력")
+
+        
